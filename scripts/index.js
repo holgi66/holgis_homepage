61e60e606f927bad3f959e7f1cefbfbe45be3466
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2021-12-13 16:37:14 +0100

    javascript eingebunden

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+function faculty_uebung( eineZahl ) {
+var n = 1; // Zum hochzaehlen
+var m = 1; // Ergebnis (Fakultät)
+while ( n <= eineZahl ) {
+m = m * n;
+n = n + 1;
+console.log(m);
+}
+return m;
+};
+
+var dieZahl = document.getElementById("eingabe").value;
+document.getElementById("ausgabe").value = faculty_uebung (dieZahl)
