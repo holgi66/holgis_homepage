4102dbc55423a6c33302f7a4fe32651a7e59f8a9
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2021-12-16 16:33:08 +0100

    20211216 Ende

---
+++ neu/index copy.js
@@ -0,0 +1,25 @@
+// ist nur ein test ob die datei greift!
+console.log("ja");
+
+function name_vorname_verknuepfen() {
+    // Eingabe
+    var vorname = document.getElementById("vorname").value;
+    var nachname = document.getElementById("nachname").value;
+    // Verknüpfen
+
+    var name = "";
+
+    if ( vorname == "" ){
+        name = nachname;
+    }
+    else {
+    name = vorname + " " + nachname;
+    }
+    
+    // Ausgabe
+    document.getElementById("name").value = name;
+}
+
+// Ausführen
+var derButton = document.getElementById("drueck_mich");
+derButton.addEventListener("click" , name_vorname_verknuepfen);               
